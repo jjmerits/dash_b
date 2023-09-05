55ac70d019af10762e8cd70052d880cf21e74451
jjmerits <35485369+jjmerits@users.noreply.github.com>
2023-09-05 13:45:35 +0900

    Update port.py

--- port.py
+++ port.py
@@ -37,7 +37,7 @@ if client_ip in allowed_ips:
     st.success('Access granted from IP: ' + client_ip)
 
     # Excel 파일 경로
-    excel_file_path = r'https://raw.githubusercontent.com/jjmerits/Dashboard/main/dash.xlsm'
+    excel_file_path = r'https://raw.githubusercontent.com/jjmerits/Dashboard/main/dash.csv'
     sheet_name = 'dashboard'
 
     # Excel 파일 읽기
