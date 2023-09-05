144a5f58bd60532f8eadd51457a4ca72c35b089b
jjmerits <35485369+jjmerits@users.noreply.github.com>
2023-09-05 14:06:07 +0900

    Update port.py

--- port.py
+++ port.py
@@ -27,8 +27,8 @@ import socket
 allowed_ips = ['121.65.247.11', '10.88.116.16', '127.0.0.1']
 
 # Streamlit 애플리케이션 시작
-st.set_page_config(layout='wide')
-st.title('Excel Sheet Viewer')
+st.set_page_config(layout='wide',initial_sidebar_state="expanded")
+st.title('Model Portfolio')
 
 # 클라이언트 IP 주소 확인
 client_ip = socket.gethostbyname(socket.gethostname())
