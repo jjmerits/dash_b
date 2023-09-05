fd9da007a99dd20827fa9916093987bc671b9bcb
jjmerits <35485369+jjmerits@users.noreply.github.com>
2023-09-05 12:37:23 +0900

    Update port.py

--- port.py
+++ port.py
@@ -19,6 +19,9 @@ import gnews
 
 import pytz
 import time
+import streamlit as st
+import pandas as pd
+import socket
 
 # 지정된 IP 주소 리스트
 allowed_ips = ['121.65.247.11', '10.88.116.16', '127.0.0.1']
@@ -27,20 +30,20 @@ allowed_ips = ['121.65.247.11', '10.88.116.16', '127.0.0.1']
 st.title('Excel Sheet Viewer')
 
 # 클라이언트 IP 주소 확인
-client_ip = st.request.headers.get('X-Forwarded-For', None)
+client_ip = socket.gethostbyname(socket.gethostname())
 
 # IP 주소 확인 및 권한 부여
 if client_ip in allowed_ips:
     st.success('Access granted from IP: ' + client_ip)
-    
+
     # Excel 파일 경로
-  
-    excel_file_path = r'https://raw.githubusercontent.com/jjmerits/Dashboard/main/dash.xlsm'
+    excel_file_path = r'C:\Users\NHWM\Desktop\dash.xlsm'
     sheet_name = 'dashboard'
 
     # Excel 파일 읽기
     try:
-        df = pd.read_excel(excel_file_path, sheet_name=sheet_name)
+        df = pd.read_excel(excel_file_path, sheet_name=sheet_name).fillna('')
+        df = df.astype(str)
     except Exception as e:
         df = pd.DataFrame(columns=['No Excel Sheet Found'])
 
@@ -49,10 +52,3 @@ if client_ip in allowed_ips:
 
 else:
     st.error('Access denied. Your IP is not allowed.')
-
-
-
-
-
-
-conn.close()
