8b36316e423d50c040d89db4a8f73c32eb8a9c83
jjmerits <35485369+jjmerits@users.noreply.github.com>
2023-09-06 13:14:43 +0900

    Update port.py

--- port.py
+++ port.py
@@ -45,6 +45,7 @@ try:
     #df = pd.read_excel(excel_file_path).fillna('')
     df = pd.read_csv(url)
     df = df.fillna('')
+    df['Value'] = df['Value'].apply(lambda x: '{:,.0f}'.format(x) if isinstance(x, (int, float)) and not pd.isnull(x) else x)
     df_style = df.style.apply(lambda row: ['background-color: lightgreen' if row['Port'] == 1 else '' for _, row in df.iterrows()], axis=1)
     df = df.astype(str)
     df = df.to_html(escape=False,index=False)
