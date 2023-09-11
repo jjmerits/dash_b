ab26d6b90af4ee122fd5f3c2959fafe5ae728f3f
jjmerits <35485369+jjmerits@users.noreply.github.com>
2023-09-11 14:15:04 +0900

    Update port.py

--- port.py
+++ port.py
@@ -54,6 +54,7 @@ try:
     new_column_names = ['DATES','NKY', 'NKY_Daily(%)', 'KOSPI200', 'KOSPI_Daily(%)', 'KOSDAQ150', 'KOSPI_Daily(%)',  'KOSDAQ150', 'KOSPI_Daily(%)','JPN_Size','JPN_Return','KR_Size','KR_Return','TW_Size','TW_Return']
     # Assign the new column names to the DataFrame
     test_df.columns = new_column_names
+    
 
         
 except Exception as e:
