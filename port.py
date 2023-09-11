f6efecf45ffce900fa3d6f5670c0214de61be79b
jjmerits <35485369+jjmerits@users.noreply.github.com>
2023-09-11 16:58:14 +0900

    Update port.py

--- port.py
+++ port.py
@@ -82,7 +82,7 @@ fig1.add_trace(go.Bar(x=test_df['DATES'], y=test_df['JPN_Return(%)'], name='JPN_
 fig1.update_xaxes(type='category', title_text='Date')
 st.plotly_chart(fig1)
 
-st.title('Korea Market')
+st.write('Korea Market')
 fig2 = go.Figure()
 
 fig2.add_trace(go.Bar(x=test_df['DATES'], y=test_df['KOSDAQ_Daily(%)'], name='KOSDAQ150'))
@@ -92,7 +92,7 @@ fig2.add_trace(go.Bar(x=test_df['DATES'], y=test_df['KR_Return(%)'], name='KR_Po
 fig2.update_xaxes(type='category', title_text='Date')
 st.plotly_chart(fig2)
 
-st.title('Taiwan Market')
+st.write('Taiwan Market')
 fig3 = go.Figure()
 
 fig3.add_trace(go.Bar(x=test_df['DATES'], y=test_df['TWSE_Daily(%)'], name='TWSE'))
