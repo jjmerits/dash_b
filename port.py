557c1c0f643dafaf13bb240e437d3fb963a1df27
jjmerits <35485369+jjmerits@users.noreply.github.com>
2023-09-11 13:28:08 +0900

    Update port.py

--- port.py
+++ port.py
@@ -20,6 +20,8 @@ import time
 import streamlit as st
 import pandas as pd
 import socket
+import plotly.express as px
+
 
 # 지정된 IP 주소 리스트
 allowed_ips = ['121.65.247.11', '10.88.116.16', '127.0.0.1']
@@ -36,6 +38,7 @@ st.title('Model Portfolio')
 # Excel 파일 경로
 
 url = st.secrets.db_credentials.ADDRESS
+url_1 = st.secrets.db_credentials.ADDRESS_1
 #excel_file_path =st.secrets.db_credentials.ADDRESS
 #sheet_name = 'dash'
 
@@ -49,11 +52,28 @@ try:
     df_style = df.style.apply(lambda row: ['background-color: lightgreen' if row['Port'] == 1 else '' for _, row in df.iterrows()], axis=1)
     df = df.astype(str)
     df = df.to_html(escape=False,index=False)
+
+    test_df = pd.read_csv(url_1)
+    test_df.columns = test_df.iloc[0]
+    test_df = test_df.iloc[1:]
+    test_df['DATES'] = pd.to_datetime(test_df['DATES'])
+    # Filter out weekends (assuming Saturday and Sunday are weekends)
+    test_df = test_df[(test_df['DATES'].dt.dayofweek != 5) & (test_df['DATES'].dt.dayofweek != 6)]
+
         
 except Exception as e:
     df = pd.DataFrame(columns=['No Excel Sheet Found'])
 
 # 데이터 표시
+st.title('Time Series Chart (Excluding Weekends)')
+st.write("Data loaded successfully. Here's the chart:")
+fig = px.bar(test_df, x='DATES', y=[test_df.columns[0], test_df.columns[2]], title='Bar Chart')
+fig.update_xaxes(title_text='Date')
+fig.update_yaxes(title_text='Values')
+
+# Display the chart in the Streamlit app
+st.plotly_chart(fig)
+
 st.write(df, unsafe_allow_html=True)
 #st.dataframe(df)
 
