317951ba684ffe7d3c112decb9d89dd129c64f5a
jjmerits <35485369+jjmerits@users.noreply.github.com>
2023-09-11 16:39:01 +0900

    Update port.py

--- port.py
+++ port.py
@@ -84,7 +84,7 @@ fig2 = go.Figure()
 
 fig2.add_trace(go.Bar(x=test_df['DATES'], y=test_df['KOSDAQ_Daily(%)'], name='KOSDAQ150'))
 fig2.add_trace(go.Bar(x=test_df['DATES'], y=test_df['KOSPI_Daily(%)'], name='KOSPI200'))
-fig2.add_trace(go.Bar(x=test_df['DATES'], y=test_df['KR_Return'], name='KR_Port_Return'))
+fig2.add_trace(go.Bar(x=test_df['DATES'], y=test_df['KR_Return(%)'], name='KR_Port_Return'))
 
 fig2.update_xaxes(type='category', title_text='Date')
 st.plotly_chart(fig2)
