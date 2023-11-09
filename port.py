4a37d53f3af182873311503f9eb6ddcbfd3a30a5
jjmerits <35485369+jjmerits@users.noreply.github.com>
2023-11-09 13:34:49 +0900

    Update port.py

--- port.py
+++ port.py
@@ -126,6 +126,7 @@ fig1 = go.Figure()
 
 fig1.add_trace(go.Bar(x=test_df['DATES'], y=test_df['NKY_Daily(%)'], name='NKY225'))
 fig1.add_trace(go.Bar(x=test_df['DATES'], y=test_df['JPN_Return(%)'], name='JPN_Port_Return'))
+fig1.add_trace(go.Bar(x=test_df['DATES'], y=test_df['JPN_Return(%)'], name='NKY_Daily(%)_adj'))
 fig1.add_trace(go.Scatter(x=test_df['DATES'], y=test_df['JP_LS_adj'], mode='lines', name='JP_Port-BM'))
 
 # Update x-axis to treat 'DATES' as a date
