00940a048675e616083fcb2c7e958cdd2295ab8e
jjmerits <35485369+jjmerits@users.noreply.github.com>
2023-11-14 17:12:06 +0900

    Update port.py

--- port.py
+++ port.py
@@ -13,6 +13,7 @@ import json
 import pytz
 import time
 import socket
+import math
 
 # 지정된 IP 주소 리스트
 allowed_ips = ['121.65.247.11', '10.88.116.16', '127.0.0.1']
@@ -141,7 +142,7 @@ performance = [
         "Name": "Nikkei225",
         "Return(%)": test_df['NKY_Cumulative_Return'].iloc[-1]*100,
         "std(%)": test_df['NKY_Daily(%)'].std()*100,
-        "Sharp": (test_df['NKY_Daily(%)'].mean() / test_df['NKY_Daily(%)'].std())*sqrt(252),
+        "Sharp": (test_df['NKY_Daily(%)'].mean() / test_df['NKY_Daily(%)'].std())*math.sqrt(252),
         "Port Return(%)": test_df['JPN_Cum_Return(%)'].iloc[-1]*100,
         "Port std(%)":(pd.to_numeric(test_df['JPN_Return(%)'].str.replace(',', ''), errors='coerce')).std()*100,
         "Port Sharp": (pd.to_numeric(test_df['JPN_Return(%)'].str.replace(',', ''), errors='coerce')).mean() / (pd.to_numeric(test_df['JPN_Return(%)'].str.replace(',', ''), errors='coerce')).std(),
