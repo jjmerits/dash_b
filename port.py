c8762039e6406f2f6152d0de2af5a623201cddc3
jjmerits <35485369+jjmerits@users.noreply.github.com>
2023-11-14 17:17:01 +0900

    Update port.py

--- port.py
+++ port.py
@@ -145,7 +145,8 @@ performance = [
         "Port Return(%)": test_df['JPN_Cum_Return(%)'].iloc[-1]*100,
         "Port std(%)":(pd.to_numeric(test_df['JPN_Return(%)'].str.replace(',', ''), errors='coerce')).std()*100,
         "Port Sharp": (pd.to_numeric(test_df['JPN_Return(%)'].str.replace(',', ''), errors='coerce')).mean() / (pd.to_numeric(test_df['JPN_Return(%)'].str.replace(',', ''), errors='coerce')).std()*math.sqrt(252),
-        "L/S Port Sharp (100%)": - (test_df['NKY_Daily(%)'] - (pd.to_numeric(test_df['JPN_Return(%)'].str.replace(',', ''), errors='coerce'))).mean() / (test_df['NKY_Daily(%)'] - (pd.to_numeric(test_df['JPN_Return(%)'].str.replace(',', ''), errors='coerce'))).std()*math.sqrt(252)
+        "L/S Port Sharp (100%)": - (test_df['NKY_Daily(%)'] - (pd.to_numeric(test_df['JPN_Return(%)'].str.replace(',', ''), errors='coerce'))).mean() / (test_df['NKY_Daily(%)'] - (pd.to_numeric(test_df['JPN_Return(%)'].str.replace(',', ''), errors='coerce'))).std()*math.sqrt(252),
+        "L/S Port Sharp (70%)": - (test_df['NKY_Daily(%)']*0.7 - (pd.to_numeric(test_df['JPN_Return(%)'].str.replace(',', ''), errors='coerce'))).mean() / (test_df['NKY_Daily(%)'] - (pd.to_numeric(test_df['JPN_Return(%)'].str.replace(',', ''), errors='coerce'))).std()*math.sqrt(252)
     },
     {
         "Name": "Kospi200",
