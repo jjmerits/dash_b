4b5dfd063f1ff4e21b9e1d59f85a768594bba2dd
jjmerits <35485369+jjmerits@users.noreply.github.com>
2023-11-14 16:11:48 +0900

    Update port.py

--- port.py
+++ port.py
@@ -143,7 +143,7 @@ performance = [
         "std(%)": test_df['NKY_Daily(%)'].std()*100,
         "Sharp": test_df['NKY_Daily(%)'].std()*100 / test_df['NKY_Daily(%)'].std(),
         "Port Return(%)": test_df['JPN_Cum_Return(%)'].iloc[-1]*100,
-        "Port std(%)":(pd.to_numeric(test_df['JPN_Return'].str.replace(',', ''), errors='coerce').diff(1).dropna() / test_df['JPN_Size'].max() ).std()*100
+        "Port std(%)":(pd.to_numeric(test_df['JPN_Return'].str.replace(',', ''), errors='coerce').diff(1).dropna() / test_df['JPN_Size'].max() ).std()*100,
         "Port Sharp": test_df['JPN_Cum_Return(%)'].iloc[-1] / test_df['JPN_Cum_Return(%)'].std(),
     },
     {
