c4c7c755bb920c1e87ffd2c90c8061156ad58197
jjmerits <35485369+jjmerits@users.noreply.github.com>
2023-11-14 16:03:26 +0900

    Update port.py

--- port.py
+++ port.py
@@ -132,7 +132,7 @@ except Exception as e:
 #st.title('Vs benchmark chart')
 st.write("9/5 수익률은 8/14일 부터의 누적 수익률")
 # Create a layout with two columns
-tt = "Port std(%)": pd.to_numeric(test_df['JPN_Return'].str.replace(',', ''), errors='coerce').diff(1)
+tt = pd.to_numeric(test_df['JPN_Return'].str.replace(',', ''), errors='coerce').diff(1)
 st.dataframe(tt)
 performance = [
     {
